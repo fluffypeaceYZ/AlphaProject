c4f61aa622a559fd5aaba3b4aa57f434ace8219a
fluffypeaceYZ <buzgata@yahoo.ca>
2016-10-17 22:51:32 -0400

    Uploading Source Code
    Final Version for now!

---
+++ Assets/BallRecognize2.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallRecognize2 : MonoBehaviour {
+
+	private static float ballgrey = 0f;
+	private static float ballwhite = 0f;
+	private static float ballorange = 0f;
+	public ParticleSystem smoke;
+	// Use this for initialization
+	void Start () {
+		 
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if(ballgrey+ballorange+ballwhite == 10f){
+
+			smoke.Play();
+			GetComponent<AudioSource>().Play();
+		}
+
+
+
+	}
+
+	void OnTriggerEnter (Collider other){
+
+		if (other.gameObject.tag == "Bomb"){
+
+			ballgrey += 3f;
+
+			print ("ball added to trigger");
+
+
+		}
+
+		if (other.gameObject.tag == "Bomborange"){
+
+			ballorange += 1.5f;
+
+
+		}
+
+		if (other.gameObject.tag == "Bombwhite"){
+
+			ballwhite += 1f;
+
+
+		}
+
+
+
+	}
+}
---
+++ Assets/PickupObject.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickupObject : MonoBehaviour {
+	GameObject mainCamera;
+	public bool carrying;
+	GameObject carriedObject;
+	public float distance;
+	GameObject throwableBomb;
+	GameObject findBomb;
+	 
+	// Use this for initialization
+	void Start () {
+		mainCamera = GameObject.FindWithTag ("MainCamera");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (carrying){
+
+			carry(carriedObject); 
+			checkDrop();
+		}
+			else
+		
+		{
+			pickup();
+		
+		}
+	}
+
+	void carry (GameObject o){ 
+	
+	o.transform.position = mainCamera.transform.position + mainCamera.transform.forward * distance;
+
+	}
+
+	void pickup(){
+
+		if (Input.GetMouseButton (0))				//Pick up Object with Mouse Button Right
+
+		{
+			int x = Screen.width /2;
+			int y = Screen.height /2;
+
+			Ray ray = mainCamera.GetComponent<Camera>().ScreenPointToRay(new Vector3(x,y));
+			RaycastHit hit;
+			if (Physics.Raycast (ray, out hit))
+
+			{
+				Pickupable p = hit.collider.GetComponent<Pickupable>();
+				if (p != null){
+
+					carrying = true;
+					carriedObject = p.gameObject;
+					p.gameObject.GetComponent<Rigidbody>().isKinematic = true;
+				}
+			}
+		}
+	}
+
+	void checkDrop(){
+
+		if (Input.GetMouseButtonUp (0)){
+
+			dropObject();
+		}
+	}
+
+	void dropObject(){
+
+		carrying = false;
+		carriedObject.GetComponent<Rigidbody>().isKinematic = false;
+		carriedObject = null;
+
+	}
+	//Throw the ball
+
+	void throwing(){
+
+		findBomb = GameObject.FindGameObjectWithTag("Bomb");
+		//throwableBomb = gameObject.GetComponent<Bomb>().Rigidbody();
+		if (Input.GetMouseButtonDown (0) && carrying == true){
+
+		gameObject.GetComponent<Rigidbody> ().AddForce (0f, 0f, 12f);
+	}
+
+ }
+
+}
